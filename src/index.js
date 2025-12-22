43a31a5ddb895f807fc5f3d9782913049db8c0e4
jckstllrd <jackstallard17@gmail.com>
2025-12-22 18:29:21 +0000

    Add dialog box for adding new projects

--- src/index.js
+++ src/index.js
@@ -18,10 +18,17 @@ setActiveProject(defaultProject);
 
 let content = document.querySelector(".content");
 
+// Create todo buttons and dialog
 let todoDialog = document.querySelector(".todo-dialog");
 let openTodoDialog = document.querySelector(".open-todo-dialog");
 let closeTodoDialog = document.querySelector(".close-todo-dialog");
-let submitTodoBtn = document.querySelector(".submit-todo");
+let submitTodoBtn = document.querySelector(".create-todo");
+
+// Create project buttons and dialog
+let projectDialog = document.querySelector(".project-dialog");
+let openProjectDialog = document.querySelector(".open-project-dialog");
+let closeProjectDialog = document.querySelector(".close-project-dialog");
+let submitProjectBtn = document.querySelector(".create-project");
 
 todoDialog.addEventListener("click", (e) => {
   const dialogDimensions = todoDialog.getBoundingClientRect();
@@ -32,6 +39,18 @@ todoDialog.addEventListener("click", (e) => {
     e.clientY > dialogDimensions.bottom
   ) {
     todoDialog.close();
+  }
+});
+
+projectDialog.addEventListener("click", (e) => {
+  const dialogDimensions = projectDialog.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    projectDialog.close();
   }
 });
 
@@ -47,13 +66,32 @@ closeTodoDialog.addEventListener("click", () => {
 });
 
 submitTodoBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    let form = e.target.form
-    let title = form.title.value 
-    let description = form.description.value
-    let dueDate = form.dueDate.value 
-    let priority = form.priority.value 
-    console.log(title, description, dueDate, priority);
-    form.reset()
-    todoDialog.close()
-})
+  e.preventDefault();
+  let form = e.target.form;
+  let title = form.title.value;
+  let description = form.description.value;
+  let dueDate = form.dueDate.value;
+  let priority = form.priority.value;
+  console.log(title, description, dueDate, priority);
+  form.reset();
+  todoDialog.close();
+});
+
+openProjectDialog.addEventListener("click", () => {
+  projectDialog.showModal();
+  console.log("opening dialog");
+});
+
+closeProjectDialog.addEventListener("click", () => {
+  ProjectDialog.close();
+});
+
+submitProjectBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let form = e.target.form;
+  let title = form.title.value;
+  let description = form.description.value;
+  console.log(title, description);
+  form.reset();
+  projectDialog.close();
+});
