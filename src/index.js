f397dad44d1af3f7c76390dda2050f24e3734d9b
jckstllrd <jackstallard17@gmail.com>
2025-12-24 11:04:59 +0000

    Refactor global code into initialisation functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./styles.css";
 
-
 class Todo {
   constructor(title, description, dueDate, priority) {
     this.title = title;
@@ -16,65 +15,44 @@ class Project {
     this.todos = [];
     this.active = false;
   }
+}
 
-  toggleActive() {
-    this.active = this.active ? false : true;
+function toggleActive(project) {
+  console.log("before toggling", project.active);
+
+  project.active = project.active ? false : true;
+
+  console.log("after toggling", project.active);
+}
+
+function setActiveProject(project) {
+  if (activeProject === undefined) {
+    toggleActive(project);
+    let activeProject = project;
+  } else {
+    toggleActive(activeProject);
+
+    toggleActive(project);
+    activeProject = project;
+    projectsDOM.textContent = "";
+    projectArray.forEach((project) => {
+      console.log(project);
+
+      displayProject(project);
+      activeProject.todos.forEach((todo) => {
+        console.log("showing todos");
+        displayTodo(todo);
+      });
+    });
   }
 }
 
-let projectArray = localStorage.getItem("projects")
-  ? JSON.parse(localStorage.getItem("projects"))
-  : [];
-
-
-console.log(projectArray.todos.length);
-  
-
-// let activeProject;
-
-if (projectArray.length == 0) {
-    console.log('no projects: adding default');
-    
-    let defaultProject = new Project('default')
-    defaultProject.toggleActive()
-    projectArray.push(defaultProject)
-}
-
-console.log('hereo;in');
-console.log(projectArray);
-
-
-for(let i = 0; i < projectArray.length; i++) {
-    console.log('here now');
-    
-    console.log('projects exist, finding active');
-    
-    if (projectArray[i].active) {
-        activeProject = projectArray[i]
-        console.log('found active project', activeProject);
-    }
-}
-
-console.log(projectArray);
-
-let todosDOM = document.querySelector(".todo-list");
-
-
-function setActiveProject(project) {
-    activeProject.toggleActive()
-    project.toggleActive()
-    activeProject = project;
-}
-
-
-
-
 function displayTodo(todo) {
-    console.log(todo);
-    
+  console.log(todo);
+
   console.log("displaying todo");
-      const todoDiv = document.createElement("div");
-    todoDiv.classList.toggle("todo");
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.toggle("todo");
 
   const todoItem = document.createElement("div");
   todoItem.classList.toggle("todo-item");
@@ -114,132 +92,184 @@ function displayTodo(todo) {
   delBtn.classList.toggle("todo-delete");
   delBtn.classList.toggle("btn");
   delBtn.textContent = "x";
-  delBtn.addEventListener("click", (e) => {removeTodo(e)})
+  delBtn.addEventListener("click", (e) => {
+    removeTodo(e);
+  });
 
-  todoDel.appendChild(delBtn)
-  todoItem.appendChild(todoDel)
-  todoDiv.appendChild(todoItem)
-  todosDOM.appendChild(todoDiv)
-
-
-  //   const title = document.createElement("p");
-  //   title.textContent = todo.title;
-  //   todoDiv.appendChild(title);
-
-  //   const description = document.createElement("p");
-  //   description.textContent = todo.description;
-  //   todoDiv.appendChild(description);
-
-  //   const dueDate = document.createElement("p");
-  //   dueDate.textContent = todo.dueDate;
-  //   todoDiv.appendChild(dueDate);
-
-  //   const priority = document.createElement("p");
-  //   priority.textContent = todo.priority;
-  //   todoDiv.appendChild(priority);
-
-  //   todosDOM.appendChild(todoDiv);
+  todoDel.appendChild(delBtn);
+  todoItem.appendChild(todoDel);
+  todoDiv.appendChild(todoItem);
+  todosDOM.appendChild(todoDiv);
 }
 
 function removeTodo(e) {
-    console.log(e);
-    
+  console.log(e);
 }
-
 
 function createNewTodo(title, description, dueDate, priority, project) {
   let todo = new Todo(title, description, dueDate, priority);
   project.todos.push(todo);
   console.log("creating todo");
-    localStorage.setItem("projects", JSON.stringify(project))
+  localStorage.setItem("projects", JSON.stringify(projectArray));
   displayTodo(todo);
 }
 
-let content = document.querySelector(".content");
+function initialiseDOM() {
+  // Create todo buttons and dialog
+  let todoDialog = document.querySelector(".todo-dialog");
+  let openTodoDialog = document.querySelector(".open-todo-dialog");
+  let closeTodoDialog = document.querySelector(".close-todo-dialog");
+  let submitTodoBtn = document.querySelector(".create-todo");
 
-// Create todo buttons and dialog
-let todoDialog = document.querySelector(".todo-dialog");
-let openTodoDialog = document.querySelector(".open-todo-dialog");
-let closeTodoDialog = document.querySelector(".close-todo-dialog");
-let submitTodoBtn = document.querySelector(".create-todo");
+  // Create project buttons and dialog
+  let projectDialog = document.querySelector(".project-dialog");
+  let openProjectDialog = document.querySelector(".open-project-dialog");
+  let closeProjectDialog = document.querySelector(".close-project-dialog");
+  let submitProjectBtn = document.querySelector(".create-project");
 
-// Create project buttons and dialog
-let projectDialog = document.querySelector(".project-dialog");
-let openProjectDialog = document.querySelector(".open-project-dialog");
-let closeProjectDialog = document.querySelector(".close-project-dialog");
-let submitProjectBtn = document.querySelector(".create-project");
+  todoDialog.addEventListener("click", (e) => {
+    const dialogDimensions = todoDialog.getBoundingClientRect();
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      todoDialog.close();
+    }
+  });
 
-todoDialog.addEventListener("click", (e) => {
-  const dialogDimensions = todoDialog.getBoundingClientRect();
-  if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
-  ) {
+  projectDialog.addEventListener("click", (e) => {
+    const dialogDimensions = projectDialog.getBoundingClientRect();
+    if (
+      e.clientX < dialogDimensions.left ||
+      e.clientX > dialogDimensions.right ||
+      e.clientY < dialogDimensions.top ||
+      e.clientY > dialogDimensions.bottom
+    ) {
+      projectDialog.close();
+    }
+  });
+
+  openTodoDialog.addEventListener("click", () => {
+    todoDialog.showModal();
+    console.log("opening dialog");
+  });
+
+  closeTodoDialog.addEventListener("click", () => {
     todoDialog.close();
-  }
-});
+  });
 
-projectDialog.addEventListener("click", (e) => {
-  const dialogDimensions = projectDialog.getBoundingClientRect();
-  if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
-  ) {
+  submitTodoBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let form = e.target.form;
+    let title = form.title.value;
+    let description = form.description.value;
+    let dueDate = form.dueDate.value;
+    let priority = form.priority.value;
+    console.log(title, description, dueDate, priority);
+    createNewTodo(title, description, dueDate, priority, activeProject);
+    form.reset();
+    todoDialog.close();
+  });
+
+  submitProjectBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let form = e.target.form;
+    let title = form.title.value;
+    let description = form.description.value;
+
+    console.log(title, description);
+    createProject(title);
+    form.reset();
     projectDialog.close();
-  }
-});
+  });
 
-console.log(todoDialog);
+  openProjectDialog.addEventListener("click", () => {
+    projectDialog.showModal();
+    console.log("opening dialog");
+  });
 
-openTodoDialog.addEventListener("click", () => {
-  todoDialog.showModal();
-  console.log("opening dialog");
-});
-
-closeTodoDialog.addEventListener("click", () => {
-  todoDialog.close();
-});
-
-submitTodoBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let form = e.target.form;
-  let title = form.title.value;
-  let description = form.description.value;
-  let dueDate = form.dueDate.value;
-  let priority = form.priority.value;
-  console.log(title, description, dueDate, priority);
-  createNewTodo(title, description, dueDate, priority, activeProject);
-  form.reset();
-  todoDialog.close();
-});
-
-openProjectDialog.addEventListener("click", () => {
-  projectDialog.showModal();
-  console.log("opening dialog");
-});
-
-closeProjectDialog.addEventListener("click", () => {
-  projectDialog.close();
-});
-
-function createProject(title, description) {
-
+  closeProjectDialog.addEventListener("click", () => {
+    projectDialog.close();
+  });
 }
 
-submitProjectBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let form = e.target.form;
-  let title = form.title.value;
-  let description = form.description.value;
+function displayProject(project) {
+  console.log("displaying project");
 
-  console.log(title, description);
-  createProject()
-  form.reset();
-  projectDialog.close();
+  const projectDiv = document.createElement("div");
+  projectDiv.classList.toggle("project");
+
+  const activeDiv = document.createElement("div");
+  activeDiv.classList.toggle("active-div");
+
+  const activeSign = document.createElement("h2");
+  activeSign.textContent = "//";
+  activeDiv.appendChild(activeSign);
+
+  const projectTitle = document.createElement("h2");
+  projectTitle.textContent = project.title;
+
+  if (project.active) {
+    projectDiv.appendChild(activeDiv);
+  }
+  projectDiv.appendChild(projectTitle);
+  projectsDOM.appendChild(projectDiv);
+}
+
+function createProject(title) {
+  let project = new Project(title);
+  setActiveProject(project);
+  displayProject(project);
+  projectArray.push();
+}
+
+function initialiseLocalStorage(activeProject) {
+  projectArray = localStorage.getItem("projects")
+    ? JSON.parse(localStorage.getItem("projects"))
+    : [];
+
+  console.log(projectArray.length);
+
+  if (projectArray.length == 0) {
+    console.log("no projects: adding default");
+
+    let defaultProject = new Project("default");
+    console.log("created default");
+
+    setActiveProject(defaultProject);
+    projectArray.push(defaultProject);
+    console.log("active project set");
+  }
+
+  for (let i = 0; i < projectArray.length; i++) {
+    console.log("here now");
+
+    console.log("projects exist, finding active");
+
+    if (projectArray[i].active) {
+      activeProject = projectArray[i];
+      console.log("found active project", activeProject);
+    }
+  }
+
+  return projectArray, activeProject;
+}
+
+// Running the app
+let activeProject;
+let projectArray;
+let todosDOM = document.querySelector(".todo-list");
+let projectsDOM = document.querySelector(".project-list");
+
+initialiseDOM();
+projectArray, activeProject = initialiseLocalStorage(activeProject);
+
+activeProject.todos.forEach((todo) => {
+  displayTodo(todo);
 });
 
-
+projectArray.forEach((project) => {
+  displayProject(project);
+});
